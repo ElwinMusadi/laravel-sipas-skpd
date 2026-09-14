595e16cae180b24abac91db261514be8eef937e0
ElwinMusadi <elwinmusadi@gmail.com>
2026-09-15 00:40:35 +0800

    Upload motion graphics dan path.

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -9,7 +9,8 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import { store } from '@/routes/login';
-import skpdMotionGraphics from '../../../../blueprint/video/Motion-graphics-skpd.mp4';
+
+const skpdMotionGraphics = '/video/Motion-graphics-skpd.mp4';
 
 type Props = {
     status?: string;
